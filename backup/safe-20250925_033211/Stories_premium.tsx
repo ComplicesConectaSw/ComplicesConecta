ea8ed86f6f48b1401a7258628bc75ae1d56278a1
ComplicesConecta <complicesconecta@gmail.com>
2025-09-25 22:49:56 -0600

    🗂️ Limpieza Final - 25/09/2025 22:47 - Directorio backup eliminado, backups consolidados en git, proyecto optimizado

--- backup/safe-20250925_033211/Stories_premium.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import StoriesContainer from "@/components/stories/StoriesContainer";
-
-const Stories = () => {
-  return <StoriesContainer />;
-};
-
-export default Stories;
